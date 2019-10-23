8c8fbaf47052fae49015dff3339a524b489387bd
r.kazakov <r.kazakov@corp.mail.ru>
2019-10-23 17:50:54 +0300

    refactoring after review

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import logging
 
 import requests
 from dotenv import load_dotenv
@@ -8,84 +9,95 @@ XKCD_URL_TEMLATE = "http://xkcd.com/{issue}/info.0.json"
 VK_API_BASE_URL = "https://api.vk.com/method/{method}"
 
 
-class VkWallPhotoPublisher:
-    def __init__(self):
-        self.group_id = os.getenv('XKCD_VK_GROUP_ID')
-        self.default_parameters = {
+class VkException(Exception):
+    pass
+
+
+def get_default_vk_parameters():
+    access_token = os.getenv('USER_ACCESS_TOKEN_VK_DVMN')
+    if access_token:
+        return {
             'v': 5.102,
-            'access_token': os.getenv('USER_ACCESS_TOKEN_VK_DVMN')
+            'access_token': access_token
         }
+    raise VkException("USER_ACCESS_TOKEN_VK_DVMN variable has not been set")
 
-    @staticmethod
-    def prep_api_url(method):
-        return VK_API_BASE_URL.format(method=method)
 
-    @staticmethod
-    def parse_response(response):
-        # check response and parse it
+def get_vk_group_id():
+    group_id = os.getenv('XKCD_VK_GROUP_ID')
+    if group_id:
+        return group_id
+    raise VkException("XKCD_VK_GROUP_ID env variable has not been set")
+
+
+def prep_api_url(method):
+    return VK_API_BASE_URL.format(method=method)
+
+
+def parse_response(response):
+    # check response and parse it
+    response.raise_for_status()
+    result = response.json()
+    if 'error' in result:
+        raise VkException(f"error code {result['error']['error_code']} {result['error']['error_msg']}")
+    return result
+
+
+def get_wall_upload_server():
+    # https://vk.com/dev/photos.getWallUploadServer
+    get_wall_upload_server_uri = prep_api_url(method='photos.getWallUploadServer')
+    parameters = {'group_id': get_vk_group_id(), **get_default_vk_parameters()}
+    resp = requests.get(get_wall_upload_server_uri, params=parameters)
+    result = parse_response(resp)
+    return result['response']['upload_url']
+
+
+def upload_photo_to_server(upload_url, file_path):
+    with open(file_path, 'rb') as fl:
+        files = {
+            'photo': fl
+        }
+        response = requests.post(upload_url, files=files)
         response.raise_for_status()
-        result = response.json()
-        if 'error' in result:
-            raise Exception(f"error code {result['error']['error_code']} {result['error']['error_msg']}")
-        return result
 
-    def photos_get_wall_upload_server(self, group_id=None):
-        # https://vk.com/dev/photos.getWallUploadServer
-        if not group_id:
-            group_id = self.group_id
+        return response.json()
 
-        get_wall_upload_server_uri = self.prep_api_url(method='photos.getWallUploadServer')
-        parameters = {'group_id': group_id, **self.default_parameters}
-        resp = requests.get(get_wall_upload_server_uri, params=parameters)
-        result = self.parse_response(resp)
-        return result['response']['upload_url']
 
-    @staticmethod
-    def upload_photo_to_server(upload_url, file_path):
-        with open(file_path, 'rb') as fl:
-            files = {
-                'photo': fl
-            }
-            response = requests.post(upload_url, files=files)
-            response.raise_for_status()
+def parse_photo_names(response):
+    # prepare filenames for https://vk.com/dev/wall.post
+    names = []
+    photos = response['response']
+    for photo in photos:
+        names = names + [f"photo{photo['owner_id']}_{photo['id']}"]
 
-            return response.json()
+    return names
 
-    @staticmethod
-    def parse_photo_names(response):
-        # prepare filenames for https://vk.com/dev/wall.post
-        names = []
-        photos = response['response']
-        for photo in photos:
-            names = names + [f"photo{photo['owner_id']}_{photo['id']}"]
 
-        return names
+def save_wall_photo(parameters):
+    # https://vk.com/dev/photos.saveWallPhoto
+    photos_save_wall_photo_uri = prep_api_url(method='photos.saveWallPhoto')
+    if 'group_id' not in parameters:
+        parameters['group_id'] = get_vk_group_id()
 
-    def photos_save_wall_photo(self, parameters):
-        # https://vk.com/dev/photos.saveWallPhoto
-        photos_save_wall_photo_uri = self.prep_api_url(method='photos.saveWallPhoto')
-        if 'group_id' not in parameters:
-            parameters['group_id'] = self.group_id
+    parameters = {**parameters,
+                  **get_default_vk_parameters()}
 
-        parameters = {**parameters,
-                      **self.default_parameters}
+    resp = requests.post(photos_save_wall_photo_uri, params=parameters)
+    return parse_response(resp)
 
-        resp = requests.post(photos_save_wall_photo_uri, params=parameters)
-        return self.parse_response(resp)
 
-    def photo_wall_post(self, parameters):
-        # https://vk.com/dev/wall.post
-        photo_wall_post_uri = self.prep_api_url(method='wall.post')
-        if 'owner_id' not in parameters:
-            parameters['owner_id'] = f"-{self.group_id}"
+def post_to_wall(parameters):
+    # https://vk.com/dev/wall.post
+    photo_wall_post_uri = prep_api_url(method='wall.post')
+    if 'owner_id' not in parameters:
+        parameters['owner_id'] = f"-{get_vk_group_id()}"
 
-        if 'from_group' not in parameters:
-            parameters['from_group'] = os.getenv('XKCD_VK_WALL_POST_FROM_GROUP', 0)
+    if 'from_group' not in parameters:
+        parameters['from_group'] = os.getenv('XKCD_VK_WALL_POST_FROM_GROUP', 0)
 
-        parameters = {**self.default_parameters, **parameters}
-        resp = requests.post(photo_wall_post_uri, data=parameters)
-        _ = self.parse_response(resp)
-        print("Comics has been published")
+    parameters = {**get_default_vk_parameters(), **parameters}
+    resp = requests.post(photo_wall_post_uri, data=parameters)
+    _ = parse_response(resp)
 
 
 def download_picture(url, picture_name=None, image_store_path=''):
@@ -101,40 +113,41 @@ def download_picture(url, picture_name=None, image_store_path=''):
     return picture_path
 
 
-def publish_comics_to_group(comics_file_path, message):
-    photo_publisher = VkWallPhotoPublisher()
-    upload_url = photo_publisher.photos_get_wall_upload_server()
-    uploaded_photo_parameters = photo_publisher.upload_photo_to_server(upload_url, comics_file_path)
-    names = photo_publisher.parse_photo_names(photo_publisher.photos_save_wall_photo(uploaded_photo_parameters))
-    comics_to_publish = {
+def publish_comic_to_group(comics_file_path, message):
+    upload_url = get_wall_upload_server()
+    uploaded_photo_parameters = upload_photo_to_server(upload_url, comics_file_path)
+    names = parse_photo_names(save_wall_photo(uploaded_photo_parameters))
+    comic_to_publish = {
         'attachments': ','.join(names),
         'message': message,
     }
-    photo_publisher.photo_wall_post(comics_to_publish)
+    post_to_wall(comic_to_publish)
 
 
-def get_comics_by_issue(issue=''):
+def get_comic_data_by_issue(issue=''):
     python_comics = XKCD_URL_TEMLATE.format(issue=issue)
     response = requests.get(python_comics)
     response.raise_for_status()
-    comics = response.json()
-    num = comics['num']
+    comic = response.json()
+    num = comic['num']
     if not issue:
         return num, None, None
 
-    image_url = comics['img']
+    image_url = comic['img']
     picture_path = download_picture(image_url)
-    title = comics['title']
+    title = comic['title']
     return num, picture_path, title
 
 
 def main():
-    latest_comics, *_ = get_comics_by_issue()
-    random_issue = random.randint(1, latest_comics)
-    _, file_path, title = get_comics_by_issue(random_issue)
+    latest_comic_issue_number, *_ = get_comic_data_by_issue()
+    random_comic_number = random.randint(1, latest_comic_issue_number)
+    _, file_path, title = get_comic_data_by_issue(random_comic_number)
 
     try:
-        publish_comics_to_group(file_path, title)
+        publish_comic_to_group(file_path, title)
+    except (VkException, requests.HTTPError) as error:
+        logging.error(f'Problems with {error}')
     finally:
         os.remove(file_path)
 
